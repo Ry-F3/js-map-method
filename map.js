58c9f10a615cc5610c42f569ad088726114b6c69
Ry-F3 <rhysfeew@gmail.com>
2023-09-04 16:36:29 +0000

    Initial Commit

--- map.js
+++ map.js
@@ -5,16 +5,31 @@
  */
 
 // Using a for loop
+let nums = [1, 2, 3, 4, 5];
+let results = [];
 
+for (let num of nums) {
+  results.push(num * 2);
+}
+
+console.log(results);
 
 // Using map()
+const multByTwo = function (num) {
+  return num * 2;
+};
 
+const mapResults = nums.map(multByTwo);
+console.log(mapResults);
 
 // Simplified w/ map()
-
+const simplified = nums.map(function(num) {return num * 2});
+console.log(simplified);
 
 // Simplfied w/ map() + arrow function
 
+const arrowSimple = nums.map(num => num *2);
+console.log(arrowSimple);
 
 // With objects:
 const students = [
@@ -37,3 +52,21 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentsWithIds = students.map(student => [student.name, student.id]);
+console.log(studentsWithIds);
+
+const ageMapping = {
+  1: 25, // Age for id 1
+  2: 30, // Age for id 2
+  3: 28, // Age for id 3
+};
+
+const studentsWithAges = students.map(student => {
+  return {
+    ...student,
+    age: ageMapping[student.id]
+  };
+});
+
+console.log(studentsWithAges);
